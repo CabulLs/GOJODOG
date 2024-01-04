cd9d48f41f638b6604c02cc02600e160abf5672b
CabulLs <106825708+CabulLs@users.noreply.github.com>
2024-01-04 09:02:23 +0700

    part 3
    ...

---
+++ scenes/menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://game.tscn")
+
+func _on_option_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().quit()
